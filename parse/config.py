4f32939a59bfe515a7c8d4b6e0289ce8206ca6ec
ChernetskiyRG <roman@softbc.ru>
2021-10-15 10:52:37 +0500

    working parser bot

--- parse/config.py
+++
@@ -1,3 +0,0 @@
-URL ='https://www.computeruniverse.net/ru/c/apparatnoe-obespechenie-i-komponenty/videokarty-pci-express?page={}'
-TOKEN = '2078959055:AAHztBccxmabj-FRHm51OWOVWqNR-TB8gh8'
-DRIVER_PATH = r'C:\Users\Roman\PycharmProjects\projects\parse\chromedriver.exe'
